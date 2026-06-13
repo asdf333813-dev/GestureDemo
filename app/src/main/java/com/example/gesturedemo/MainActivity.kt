bc1ab7537d58d28ff649b78c509575ead38f66f1
Андрей <asdf333813@gmail.com>
2026-06-14 00:18:10 +0700

    Add MultiTouchDemo scale gesture example

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
@@ -34,38 +38,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    ScrollModifiers(modifier)
+    MultiTouchDemo(modifier)
 }
 
 @Composable
-fun ScrollModifiers(modifier: Modifier = Modifier) {
+fun MultiTouchDemo(modifier: Modifier = Modifier) {
 
-    val image = ImageBitmap.imageResource(
-        id = R.drawable.vacation
-    )
+    var scale by remember { mutableStateOf(1f) }
+
+    val state = rememberTransformableState { scaleChange, _, _ ->
+        scale *= scaleChange
+    }
 
     Box(
-        modifier = modifier
-            .size(150.dp)
-            .verticalScroll(rememberScrollState())
-            .horizontalScroll(rememberScrollState())
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
     ) {
-
-        Canvas(
-            modifier = Modifier.size(
-                width = 360.dp,
-                height = 270.dp
-            )
-        ) {
-
-            drawImage(
-                image = image,
-                topLeft = Offset(
-                    x = 0f,
-                    y = 0f
+        Box(
+            modifier = Modifier
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale
                 )
-            )
-        }
+                .transformable(state = state)
+                .background(Color.Blue)
+                .size(100.dp)
+        )
     }
 }
 

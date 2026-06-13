10cead179fabc1b85500d6c4f75a6577d2bd0975
Андрей <asdf333813@gmail.com>
2026-06-14 00:08:23 +0700

    Add PointerInputDrag gesture example

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
@@ -19,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -40,27 +39,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    DragDemo(modifier)
+    PointerInputDrag(modifier)
 }
 
 @Composable
-fun DragDemo(modifier: Modifier = Modifier) {
+fun PointerInputDrag(modifier: Modifier = Modifier) {
 
     Box(modifier = modifier.fillMaxSize()) {
 
         var xOffset by remember { mutableStateOf(0f) }
+        var yOffset by remember { mutableStateOf(0f) }
 
         Box(
-            modifier = Modifier
-                .offset { IntOffset(xOffset.roundToInt(), 0) }
-                .size(100.dp)
+            Modifier
+                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
                 .background(Color.Blue)
-                .draggable(
-                    orientation = Orientation.Horizontal,
-                    state = rememberDraggableState { distance ->
-                        xOffset += distance
+                .size(100.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { _, distance ->
+                        xOffset += distance.x
+                        yOffset += distance.y
                     }
-                )
+                }
         )
     }
 }
